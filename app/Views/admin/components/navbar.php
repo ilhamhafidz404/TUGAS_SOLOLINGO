b9057094889dba4e0543f64c6c31b67b89885e7d
ilhamhafidz404 <xxspanzie@gmail.com>
2024-05-26 14:15:19 +0700

    Template stisla Dashboard Admin

---
+++ app/Views/admin/components/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-secondary navbar-expand-lg">
+  <div class="container">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a href="#" class="nav-link">
+          <i class="fas fa-fire"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
